c096ee340b832a7a1f1bea8a270079166382276b
kenwoo9y <kenwoo9y@gmail.com>
2024-01-23 22:34:21 +0900

    ITP1_9_C

---
+++ itp1_9/c_card_game.py
@@ -0,0 +1,17 @@
+n = int(input())
+
+taro_point = 0
+hanako_point = 0
+
+for i in range(n):
+    taro_card, hanako_card = input().split()
+    
+    if taro_card > hanako_card:
+        taro_point += 3
+    elif hanako_card > taro_card:
+        hanako_point += 3
+    else:
+        taro_point += 1
+        hanako_point += 1
+
+print(taro_point, hanako_point)
